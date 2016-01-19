7dfaf0dc0b689001a5805597e007d707507838ae
Marcin Badurowicz <m@badurowicz.net>
2016-01-19 22:50:34 +0100

    build scripts for Windows

---
+++ scripts/build-windows.bat
@@ -0,0 +1,56 @@
+@echo off
+IF /I "%NSBUILDENV%"=="" GOTO :BADENV
+SET FLAVOR=release
+IF /I "%1"=="DEBUG" SET FLAVOR=debug
+
+rem Set PATH with Qt, MinGW and additional tools we need
+set PATH=%PATH%;%QTPATH%;%MINGWPATH%;%NSBUILDENV%\bin
+
+rem Checkout to master
+git checkout master
+call scripts\set-version.cmd
+qmake "CONFIG+=win32"
+make %FLAVOR%
+
+rem Automatic makefile does not work somehow with bison generated files
+rem so we are changing names here ourselves and calling make again
+
+IF ERRORLEVEL 1 GOTO :FIXBISON
+
+:PACK
+IF /I "%FLAVOR%"=="DEBUG" GOTO :EOF
+
+copy nsbuilder_pl.qm release\
+copy %QTPATH%\QtCore4.dll release\
+copy %QTPATH%\QtGui4.dll release\
+copy %QTPATH%\QtNetwork4.dll release\
+copy %QTPATH%\QtXml4.dll release\
+copy %MINGWPATH%\mingwm10.dll release\
+copy %MINGWPATH%\libgcc_s_dw2-1.dll release\
+del release\qrc_nsbuilder.cpp
+
+SET /P VERSION=<SVN_VERSION_FILE
+ren release nsbuilder1-%VERSION%-windows
+7z a release/nsbuilder1-%VERSION%-windows.zip nsbuilder1-%VERSION%-windows/*
+del /s /q nsbuilder1-%VERSION%-windows
+rmdir nsbuilder1-%VERSION%-windows
+
+GOTO :EOF
+
+:FIXBISON
+echo Fixing BISON Qt error
+ren imp.tab.h imp_yacc.h
+ren imp.tab.c imp_yacc.cpp
+make
+
+GOTO :PACK
+
+:BADENV
+echo Bad environment. Add NSBUILDENV environment variable with paths to:
+echo     GnuWin32: date, bison, flex, libiconv, regex, libintl
+echo Add QTPATH environment variable with path to Qt 4.7.4\bin
+echo Add MINGWPATH environment variable with path to MinGW 4.4.0\bin
+echo 7-Zip CommandLine is also required in PATH
+echo.
+
+:EOF
